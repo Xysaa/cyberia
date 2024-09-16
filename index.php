e7333efe6d90474a2f07b161c4c804e6abd25a80
Stevanus Cahya Anggara <stevanuschyaanggara@gmail.com>
2024-09-16 20:45:42 +0700

    test

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include "../config/connection.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CYBERIA 23</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="box">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, rem et deserunt harum ipsa quibusdam enim at provident eos ut!</p>
+        </div>
+    </div>
+</body>
+</html>
